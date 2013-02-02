e6e4164863b38c05cb1b4c1a1f186f0b4a718e35
James Stubblefield <jameswilliamiii@gmail.com>
2013-02-02 02:56:08 -0600

    refactored bigger element

--- bigger_element.rb
+++ bigger_element.rb
@@ -1,7 +1,7 @@
 def first_even(items)
-  even_array = items.select { |i| (i & 1) == 0 }
-  even_array.first
+  even_array = items.select { |i| (i & 1) == 0 }.first
+
 end
 
-a = [1, 3, 5, 7, 9, 0]
+a = [1, 3, 5, 7, 9, 2]
 puts first_even(a)
